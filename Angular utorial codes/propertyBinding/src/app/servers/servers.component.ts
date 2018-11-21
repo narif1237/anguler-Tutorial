aa70e5409bbb937a2d2a68bbf308115ba5907fb5
narif1237 <narif1237@gmail.com>
2018-11-22 02:14:16 +0530

    added $event at servers.ts

--- Angular utorial codes/propertyBinding/src/app/servers/servers.component.ts
+++ Angular utorial codes/propertyBinding/src/app/servers/servers.component.ts
@@ -10,18 +10,22 @@ export class ServersComponent implements OnInit {
   server: number;
   allowNewServer = false;
   serverStatus: string;
+  serverName: string;
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
 
     }, 2000);
-   }
+  }
 
   ngOnInit() {
     this.server = 45;
   }
   onclickServerStatus() {
     this.serverStatus = 'server created';
+  }
+  updateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 
 }
